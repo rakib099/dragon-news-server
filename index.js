fc207cf00fb8c33d4bde265de462614342c25371
Rakib099 <rakibnabi97@gmail.com>
2022-12-02 21:48:06 +0600

    auth recap & data load (module-60)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 // make api
 app.get('/', (req, res) => {
@@ -15,6 +16,27 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
     res.send(categories);
 })
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === "08") {
+        res.send(news);
+    }
+    else{
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', ({params}, res) => {
+    const id = params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+}) 
 
 app.listen(port, () => {
     console.log("Dragon News Server running on port:", port);
